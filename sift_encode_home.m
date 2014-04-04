226bfa6f001b082fe86b4636ece56e4bd570198f
Sang Phan <plsang@nii.ac.jp>
2014-04-04 14:52:34 +0900

    supports both FV and BOW features

--- sift_encode_home.m
+++ sift_encode_home.m
@@ -1,132 +1,132 @@
-function [ output_args ] = sift_encode_home( proj_name, codebook_size, spm, sift_algo, param, start_seg, end_seg )
-%ENCODE Summary of this function goes here
-%   Detailed explanation goes here
-%% kf_dir_name: name of keyframe folder, e.g. keyframe-60 for segment length of 60s   
+function [ output_args ] = sift_encode_home( proj_dir, szPat, sift_algo, param, dimred, codebook_size, spm, start_class, end_class)
 
 	% update: Jun 25th, SPM suported
     % setting
     set_env;
-
-    fea_dir = sprintf('/net/per900a/raid0/plsang/%s/feature/%s', proj_name, kf_dir_name);
-    if ~exist(fea_dir, 'file'),
-            mkdir(fea_dir);
-    end
         
     % encoding type
-    enc_type = 'kcb';
+    enc_type = 'soft';
 	
 	if ~exist('codebook_size', 'var'),
-		codebook_size = 4000;
+		codebook_size = 1000;
 	end
-    
-	feature_ext = sprintf('%s.%s.sift.Soft-%d-VL2.%s.devel', sift_algo, num2str(param), codebook_size, proj_name);
+   
+	if ~exist('clustering', 'var'),
+		clustering = 'kmeans';
+	end
+      
+	if ~exist('spm', 'var'),
+		spm = 0;
+	end
+	
+	default_dim = 128;
+	if ~exist('dimred', 'var'),
+		dimred = default_dim;
+	end
+	
+	feature_ext = sprintf('%s.%s.sift.cb%d', sift_algo, num2str(param), codebook_size);
 	if spm > 0,
 		feature_ext = sprintf('%s.spm', feature_ext);
 	end
 	
-    output_dir = sprintf('%s/%s.%s/%s', fea_dir, feature_ext, enc_type, szPat) ;
+	if dimred < default_dim,,
+		feature_ext = sprintf('%s.pca%d', feature_ext, dimred);
+	end
+	
+    output_dir = sprintf('%s/feature/%s.%s/%s', proj_dir, feature_ext, enc_type, szPat) ;
     if ~exist(output_dir, 'file'),
 		mkdir(output_dir);
 	end
     
-    codebook_file = sprintf('/net/per900a/raid0/plsang/%s/feature/bow.codebook.%s.devel/%s.%s.sift/data/codebook.%d.mat', proj_name, proj_name, sift_algo, num2str(param), codebook_size);
-	
+    codebook_file = sprintf('%s/feature/bow.codebook.devel/%s.%s.sift/data/codebook.%s.%d.%d.mat', ...
+		proj_dir, sift_algo, num2str(param), clustering, codebook_size, dimred);
+		
 	fprintf('Loading codebook [%s]...\n', codebook_file);
     codebook_ = load(codebook_file, 'codebook');
     codebook = codebook_.codebook;
-    
-	kdtree = vl_kdtreebuild(codebook);
-	
-    [segments, sinfos, vinfos] = load_segments(proj_name, szPat, kf_dir_name);
-    
-    if ~exist('start_seg', 'var') || start_seg < 1,
-        start_seg = 1;
-    end
-    
-    if ~exist('end_seg', 'var') || end_seg > length(segments),
-        end_seg = length(segments);
-    end
-    
-    %tic
-	
-    kf_dir = sprintf('/net/per900a/raid0/plsang/%s/keyframes/%s', proj_name, szPat);
-	
-	if strcmp(proj_name, 'trecvidmed10'),
-		kf_dir = sprintf('/net/per900a/raid0/plsang/%s/keyframes', proj_name);
+ 
+	kdtree = []; 
+	if strcmp(clustering, 'kmeans'),
+		kdtree = vl_kdtreebuild(codebook);
 	end
+	
+ 	low_proj = []; %% used along with fisher vector encoding
+	if dimred < default_dim,
+		lowproj_file = sprintf('%s/feature/bow.codebook.devel/%s.%s.sift/data/lowproj.%d.%d.mat', ...
+			proj_dir, sift_algo, num2str(param), dimred, default_dim);
+			
+		fprintf('Loading low projection matrix [%s]...\n', lowproj_file);
+		low_proj_ = load(lowproj_file, 'low_proj');
+		low_proj = low_proj_.low_proj;
+	end
+	
+	root_kf_dir = '/net/per610a/export/das09f/satoh-lab/dungmt/DataSet/LSVRC/2010/image';
+    kf_dir = sprintf('%s/%s', root_kf_dir, szPat);
     
-		
-    parfor ii = start_seg:end_seg,
-        segment = segments{ii};                 
+	train_classes = dir (fullfile(kf_dir, 'n*'));
+	
+	if ~exist('start_class', 'var') || start_class < 1,
+        start_class = 1;
+    end
     
-        pattern =  '(?<video>\w+)\.\w+\.frame(?<start>\d+)_(?<end>\d+)';
-        info = regexp(segment, pattern, 'names');
-        
-        output_file = [output_dir, '/', info.video, '/', segment, '.mat'];
-        if exist(output_file, 'file'),
-            fprintf('File [%s] already exist. Skipped!!\n', output_file);
-            continue;
-        end
-        
-        video_kf_dir = fullfile(kf_dir, info.video);
-        
-        start_frame = str2num(info.start);
-        end_frame = str2num(info.end);
-        
-		kfs = dir([video_kf_dir, '/*.jpg']);
-       
-		%% update Jul 5, 2013: support segment-based
-		max_frames = max(vinfos.(info.video));
-		max_keyframes = length(kfs);
+    if ~exist('end_class', 'var') || end_class > length(train_classes),
+        end_class = length(train_classes);
+    end
+	
+    for ii = start_class:end_class,
+        class_name = train_classes(ii).name;              
+    
+        class_kf_dir = fullfile(kf_dir, class_name);
 		
-        start_kf = floor(start_frame*max_keyframes/max_frames) + 1;
-		end_kf = floor(end_frame*max_keyframes/max_frames);
+		kfs = dir([class_kf_dir, '/*.JPEG']);
+        
+		output_file = [output_dir, '/', class_name, '.mat'];
+		if exist(output_file, 'file'),
+			fprintf('File [%s] already exist. Skipped!!\n', output_file);
+			continue;
+		end
 		
-		fprintf(' [%d --> %d --> %d] Extracting & encoding for [%s - %d/%d kfs (%d - %d)]...\n', start_seg, ii, end_seg, segment, end_kf - start_kf + 1, max_keyframes, start_kf, end_kf);
+		codes = cell(length(kfs), 1);
 		
-        code = [];
-		for jj = start_kf:end_kf,
-			if ~mod(jj, 10),
+		fprintf(' [%d/%d] Extracting and Encoding for image class [%s]...\n', ii - start_class + 1, end_class - start_class + 1, class_name);
+		
+		parfor jj = 1:length(kfs),
+			if ~mod(jj, 50),
 				fprintf('%d ', jj);
 			end
-			img_name = kfs(jj).name;
-			img_path = fullfile(video_kf_dir, img_name);
 			
+			img_name = kfs(jj).name;
+			
+			img_path = fullfile(class_kf_dir, img_name);
+			
+			%% error for image: n02872752_3721.JPEG (class 284)
+			%% JPEG images with CMYK colorspace are not currently supported.
+
 			try
 				im = imread(img_path);
+				
+				[frames, descrs] = sift_extract_features( im, sift_algo, param );
+				
+				% if more than 50% of points are empty --> possibley empty image
+				if sum(all(descrs == 0, 1)) > 0.5*size(descrs, 2),
+					warning('Maybe blank image...[%s]. Skipped!\n', img_name);
+					continue;
+				end
+				
+				if spm > 0
+					code = sift_encode_spm(enc_type, size(im), frames, descrs, codebook, kdtree, low_proj);
+				else
+					code = sift_do_encoding(enc_type, descrs, codebook, kdtree, low_proj);	
+				end
+				
 			catch
-				warning('Error while reading image [%s]!!\n', img_path);
 				continue;
-			end
+			end			
 			
-			[frames, descrs] = sift_extract_features( im, sift_algo, param )
-            
-            % if more than 50% of points are empty --> possibley empty image
-            if sum(all(descrs == 0, 1)) > 0.5*size(descrs, 2),
-                warning('Maybe blank image...[%s]. Skipped!\n', img_name);
-                continue;
-            end
-			
-			if spm > 0
-				code_ = sift_encode_spm(size(im), frames, descrs, codebook, kdtree, enc_type);
-			else
-				code_ = kcb_encode(descrs, codebook, kdtree);	
-			end
-			
-			code = [code code_];
+			codes{jj} = code;
 		end
-		fprintf('\n');
-		% averaging...
-		code = mean(code, 2);
-                
-        % output code
-        output_vdir = [output_dir, '/', info.video];
-        if ~exist(output_vdir, 'file'),
-            mkdir(output_vdir);
-        end
-        
-        
-        par_save(output_file, code); % MATLAB don't allow to save inside parfor loop             
+		
+        par_save(output_file, codes); % MATLAB don't allow to save inside parfor loop             
         
     end
     
@@ -135,15 +135,14 @@ function [ output_args ] = sift_encode_home( proj_name, codebook_size, spm, sift
 
 end
 
-function par_save( output_file, code )
-  save( output_file, 'code', '-v7.3');
+function par_save( output_file, codes )
+  save( output_file, 'codes');
 end
 
 function log (msg)
 	fh = fopen('sift_encode_home.log', 'a+');
-    msg = [msg, ' at ', datestr(now)];
+    msg = [msg, ' at ', datestr(now), '\n'];
 	fprintf(fh, msg);
-	fprintf(fh, '\n');
 	fclose(fh);
 end
 
